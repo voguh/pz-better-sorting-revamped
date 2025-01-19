6ff1e7d827239e6d3a633496685108764c54a58e
Voguh <voguhofc@protonmail.com>
2025-01-19 13:12:11 -0300

    feat: add support for Arithmechick's Convert Radios to AC Power mod

---
+++ Contents/mods/BetterSortingRevamped/media/lua/client/Mod Support/TSW_RadiosToAC_Categories.lua
@@ -0,0 +1,9 @@
+-- Arithmechick's Convert Radios to AC Power
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=2857191956
+if getActivatedMods():contains("TSW_RadiosToAC") then
+    BetterSorting.CategorizeItem("RadiosToAC.HamRadio1_AC", BS_CATEGORIES.ELEC);
+    BetterSorting.CategorizeItem("RadiosToAC.HamRadio2_AC", BS_CATEGORIES.ELEC);
+    BetterSorting.CategorizeItem("RadiosToAC.HamRadioMakeShift_AC", BS_CATEGORIES.ELEC);
+    BetterSorting.CategorizeItem("RadiosToAC.RadioBlack_AC", BS_CATEGORIES.ELEC);
+    BetterSorting.CategorizeItem("RadiosToAC.RadioRed_AC", BS_CATEGORIES.ELEC);
+end
